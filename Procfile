adc2fb44d810190a3535097dcbdc07c2fc53530e
dlin <lindevin101@gmail.com>
2023-12-13 17:18:04 -0500

    deploy ready

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pets_api.wsgi
--- pets/models.py
+++ pets/models.py
@@ -45,4 +45,4 @@ class Following(models.Model):
 class Like(models.Model):
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)    
+    created_at = models.DateTimeField(auto_now_add=True)
